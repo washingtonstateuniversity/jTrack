dc4c9e00e38dc2bcbac9a43753c19a3ba0485079
jeremyBass <jeremybass@cableone.net>
2014-09-26 15:43:49 -0700

    keep load_script in scoope

--- src/jtrack.js
+++ src/jtrack.js
@@ -206,10 +206,10 @@ var jtrackedOptions=[];
 			type: "GET",
 			url: src,
 			success: function() {  
-                if (!defined(_gat)) {
+				if (!defined(_gat)) {
 					debug('!!!!!! _gat has NOT been defined !!!!!!!');
-                    throw "_gat has not been defined";
-                }  else {
+					throw "_gat has not been defined";
+				}  else {
 					debug('!!!!!! loaded the GA code !!!!!!!');
 			  		init_analytics(account_id,callback); 
 				}
@@ -217,14 +217,14 @@ var jtrackedOptions=[];
 			dataType: "script",
 			cache: true // We want the cached version
 		  });
-		};  
+		};
+		// Enable tracking when called or on page load?
+		if(settings.onload === true || settings.onload === null) {
+			jQuery(document).ready(function(){load_script();});
+		} else {
+		  load_script();
+		}
 	}
-    // Enable tracking when called or on page load?
-    if(settings.onload === true || settings.onload === null) {
-		jQuery(document).ready(function(){load_script();});
-    } else {
-      load_script();
-    }
   };
 
   /**
