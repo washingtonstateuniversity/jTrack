f666fc40c98304a1ee5e0fb2f7aeab0c5d258018
Jeremy Bass <jeremybass@cableone.net>
2014-12-19 08:30:51 -0800

    clean up tab sp mix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,38 @@
 module.exports = function(grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
-        jshint: {
-            src: ['src/*.js', 'src/config.js', 'tests/*.js'],
-            options: {
-                smarttabs: true,
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                boss: true,
-                eqnull: true,
-                browser: true,
-                globals: {
-                    require: true,
-                    define: true,
-                    requirejs: true,
-                    describe: true,
-                    expect: true,
-                    it: true,
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		},
+		jshint: {
+			src: ['src/*.js', 'src/config.js', 'tests/*.js'],
+			options: {
+				smarttabs: true,
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				browser: true,
+				globals: {
+					require: true,
+					define: true,
+					requirejs: true,
+					describe: true,
+					expect: true,
+					it: true,
 					_gat: true,
 					jQuery: true,
 					console: true,
@@ -40,23 +40,23 @@ module.exports = function(grunt) {
 					document: true,
 					window:true
 				}
-            }
-        },
-        watch: {
-            files: '<%= jshint.src %>',
-            tasks: ['jshint']
-        }
-    });
+			}
+		},
+		watch: {
+			files: '<%= jshint.src %>',
+			tasks: ['jshint']
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    
-    // Load JSHint task
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    
-    // Default task(s).
-    grunt.registerTask('default', ['jshint','uglify']);
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Load JSHint task
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// Default task(s).
+	grunt.registerTask('default', ['jshint','uglify']);
 
 };
